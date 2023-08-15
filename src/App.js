54082830602dffd37cdc4002fb67e669663c1334
Paul Fadayo <paulfadayo@yahoo.com>
2023-08-15 22:26:58 +0100

    added functionalities

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function App() {
+  const [goals, setGoals] = useState([]);
+
   return (
     <div className="App">
       <div className="container">
         <Nav />
-        <Form />
+        <Form goals={goals} setGoals={setGoals} />
 
-        <GoalTracker />
+        <GoalTracker setGoals={setGoals} goals={goals} />
       </div>
     </div>
   );
@@ -25,39 +27,59 @@ function Nav() {
   );
 }
 
-function Form() {
+function Form({ setGoals, goals }) {
+  const [add, setAdd] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newGoals = {
+      description: add,
+      id: new Date().getMilliseconds(),
+    };
+
+    setGoals([newGoals, ...goals]);
+  }
+
   return (
-    <div className="form-container">
+    <form className="form-container" onSubmit={handleSubmit}>
       <div className="form">
-        <input type="text" placeholder="type here" />
+        <input
+          type="text"
+          placeholder="type here"
+          value={add}
+          onChange={(e) => {
+            setAdd(e.target.value);
+          }}
+        />
         <button className="btn">add</button>
       </div>
-    </div>
+    </form>
   );
 }
 
-function GoalTracker() {
+function GoalTracker({ goals, setGoals }) {
+  function clicked(element) {
+    const { id, description } = element;
+
+    const filtered = goals.filter((goal) => goal.id !== id);
+
+    const confirmed = window.confirm(
+      `Are you sure you want to delete ${description}?`
+    );
+
+    confirmed && setGoals(filtered);
+  }
+
   return (
     <div className="goals">
-      <div className="goal">
-        <input type="checkbox" className="checkbox-input" />
-        <h3 className="goal-value">First Goal</h3>
-      </div>
-
-      <div className="goal">
-        <input type="checkbox" className="checkbox-input" />
-        <h3 className="goal-value">Second Goal</h3>
-      </div>
-
-      <div className="goal">
-        <input type="checkbox" className="checkbox-input" />
-        <h3 className="goal-value">Third Goal</h3>
-      </div>
-
-      <div className="goal">
-        <input type="checkbox" className="checkbox-input" />
-        <h3 className="goal-value">Fourth Goal</h3>
-      </div>
+      {goals.map((goal) => (
+        <div key={goal.id} onClick={() => clicked(goal)} className="goal">
+          <input type="checkbox" className="checkbox-input" />
+          <h3 className="goal-value">{goal.description}</h3>
+          <p className="delete">❌</p>
+        </div>
+      ))}
     </div>
   );
 }
